9f8059c98a842855bcc7ff069dfafa343b30e549
Lewis Remington <s5063237@bournemouth.ac.uk>
2018-09-25 18:04:40 +0100

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <SDL2/SDL.h>
-
+#include <GL/glew.h>
 #include <exception>
 
 #define WINDOW_WIDTH 640
@@ -14,9 +14,51 @@ int main(int argc, char *argv[])
 
   SDL_Window *window = SDL_CreateWindow("Triangle",
     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-    WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
+    WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+
+  if (!SDL_GL_CreateContext(window))
+  {
+	  throw std::exception();
+  }
+  if (glewInit() != GLEW_OK)
+  {
+	  throw std::exception();
+  }
 
   bool quit = false;
+  
+  //CODE
+    const GLfloat positions[] = {
+	  0.0f, 0.5f, 0.0f,
+	  -0.5f, -0.5f, 0.0f,
+	  0.0f, -0.5f, 0.0f
+  };
+
+	GLuint positionsVboId = 0;
+
+	glGenBuffers(1, &positionsVboId);
+	if (!positionsVboId)
+	{
+		throw std::exception();
+	}
+	glBindBuffer(GL_ARRAY_BUFFER, positionsVboId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	GLuint vaoId = 0;
+	glGenVertexArrays(1, &vaoId);
+	if (!vaoId)
+	{
+		throw std::exception();
+	}
+	glBindVertexArray(vaoId);
+	glBindBuffer(GL_ARRAY_BUFFER, positionsVboId);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	// note -> start of page 5 3dgp-lab1_2-triangle
 
   while(!quit)
   {
@@ -24,6 +66,18 @@ int main(int argc, char *argv[])
 
     while(SDL_PollEvent(&event))
     {
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		SDL_GL_SwapWindow(window);
+
+
+
+
+
+
+
+
+
       if(event.type == SDL_QUIT)
       {
         quit = true;
